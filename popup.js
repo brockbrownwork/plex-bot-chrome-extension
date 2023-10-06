111111dad4475204a8f499cab6f88ed4da604481
Brock Brown <brockrbrown@gmail.com>
2023-10-06 08:31:48 -0400

    Some changes to the popup

--- popup.js
+++ popup.js
@@ -1,5 +1,21 @@
 var foo;
 
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  var currentTab = tabs[0]; 
+  var currentURL = currentTab.url;
+  console.log(currentURL);
+
+  if (currentURL.includes("plex.com/ControlPanel/Dashboard")) {
+      document.getElementById("message").textContent = "You're on the dashboard";
+  } else if (currentURL.includes("/ControlPanel/Production")) {
+      document.getElementById("message").textContent = "You're on the Production page";
+  } else if (currentURL.includes("/Platform/LaunchPage")) {
+      document.getElementById("message").textContent = "You're on the Platform Launch Page";
+  }
+});
+
+
+
 chrome.storage.local.get("count", (result) => {
   if (result.count === undefined) {
     chrome.storage.local.set({"count": 0});
