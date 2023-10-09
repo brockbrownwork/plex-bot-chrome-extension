7871de0c19520c3ca3fd5c964354d3e6de7fb3cb
Brown <BBrown@jewels.com>
2023-10-09 11:19:48 -0400

    Half the size of the original images so we can put a bunch on.

--- clipboard_to_barcodes.py
+++ clipboard_to_barcodes.py
@@ -36,6 +36,39 @@ for i, code in enumerate(codes):
     CODE128 = barcode.get_barcode_class('code128')
     my_code = CODE128(code, writer=ImageWriter())
 
-    filename = os.path.join(output_directory, f'barcode_{i}.png')
+    filename = os.path.join(output_directory, f'barcode_{i}')
     my_code.save(filename)
     print(f"Saved: {filename}")
+
+from PIL import Image
+import os
+
+# Define the directory where your images are stored
+input_folder = "barcode_output"
+
+# Iterate through each file in the directory
+for filename in os.listdir(input_folder):
+    # Check if the file is an image (based on the file extension)
+    if filename.endswith('.jpg') or filename.endswith('.png') or filename.endswith('.jpeg'):
+
+        # Construct the full path of the file
+        filepath = os.path.join(input_folder, filename)
+        
+        # Open the image using Pillow
+        with Image.open(filepath) as img:
+            # Get the current size of the image
+            width, height = img.size
+
+            # Compute the new size (halve the width and height)
+            new_size = (width // 2, height // 2)
+
+            # Resize the image to the new size
+            img_resized = img.resize(new_size)
+
+            # Save the resized image back to the same location (overwriting the original)
+            img_resized.save(filepath)
+
+        print(f"Resized {filename} to half its original size")
+
+print("All images have been resized!")
+
